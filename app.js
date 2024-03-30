16b1e6a77ecd47e1e611788c7278ffd8a2de70cc
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-03-30 15:38:09 +0530

    project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+// Import the Express.js library
+const express = require('express');
+
+// Create an instance of Express
+const app = express();
+
+// Define a route that responds with "Hello, World!" when accessed
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+// Export the Express app instance
+module.exports = app;
