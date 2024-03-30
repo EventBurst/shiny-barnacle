90e636e4f0a9c3efd909fd02f0ea2e527439de27
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-03-30 17:57:56 +0530

    Adedd bcrypt and jwt functionality

--- models/organizeModel.js
+++ models/organizeModel.js
@@ -1,4 +1,6 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const organizerSchema = new Schema(
   {
     email: {
@@ -23,4 +25,34 @@ const organizerSchema = new Schema(
   },
   { timestamps: true }
 );
+organizerSchema.pre("save", async function (next) {
+  if (this.isModified("password")) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+organizerSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+organizerSchema.methods.generateAccessToken = function () {
+ return jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn: process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
+organizerSchema.methods.generateRefreshToken = function () {
+ return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    { expiresIn: process.env.REFRESH_TOKEN_EXPIRY }
+  );
+};
+
 export const Organizer = mongoose.model("Organizer", organizerSchema);
