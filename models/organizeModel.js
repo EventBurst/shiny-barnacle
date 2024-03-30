c9390a9cf0bf5e4f4494a8caa7ff72269c5e91f9
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-03-30 17:26:14 +0530

    Added organizer Model

---
+++ models/organizeModel.js
@@ -0,0 +1,26 @@
+import mongoose, { Schema } from "mongoose";
+const organizerSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+export const Organizer = mongoose.model("Organizer", organizerSchema);
