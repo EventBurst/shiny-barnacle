b044969b3583680c0112a272bfb53ed136c642c3
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-01 22:45:14 +0530

    Resolved #18

--- routes/speaker.routes.js
+++ routes/speaker.routes.js
@@ -6,12 +6,20 @@ const router= Router();
 // Defining the routes
 
 // Get all speakers
+//Path: http://localhost:8002/api/shiny-barnacle/speaker/get-all
 router.get('/get-all', getSpeakers);
+
 // Create a new speaker
+//Path: http://localhost:8002/api/shiny-barnacle/speaker/create
 router.post('/create', createSpeaker);
+
 // Update a speaker
+//Path: http://localhost:8002/api/shiny-barnacle/speaker/update/:id
 router.put('/update/:id', updateSpeaker);
+
 // Delete a speaker
+//Path: http://localhost:8002/api/shiny-barnacle/speaker/delete/:id
 router.delete('/delete/:id', deleteSpeaker);
+
 // Exporting the router
 export default router;
