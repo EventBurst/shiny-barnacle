f37f5a760a6b8b3bf6a937044289dcf35915d9e2
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-01 20:16:55 +0530

    add sponsor model resolves #12

---
+++ models/sponsor.model.js
@@ -0,0 +1,36 @@
+// Require Mongoose
+import { Schema, model } from "mongoose";
+
+// Define the Sponsor schema
+const sponsorSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  phoneNumber: {
+    type: String,
+    required: true,
+  },
+  sponsorID: {
+    type: String,
+    required: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+  contribution: {
+    type: Number,
+    required: true,
+  },
+});
+
+// Create the Sponsor model
+const Sponsor = model("Sponsor", sponsorSchema);
+
+// Export the Sponsor model
+export default Sponsor;
