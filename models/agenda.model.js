047b229974b624ad3601052f5722e9abd81686da
Nikhil Sarwara <nikhil4818.be22@chitkara.edu.in>
2024-04-01 23:14:10 +0530

    fix agenda model

--- models/agenda.model.js
+++ models/agenda.model.js
@@ -1,32 +1,24 @@
-import mongoose,{Schema} from "mongoose";
-const agendaSchema = new Schema({
-    description:{
-        type:String,
-        required:true,
+import mongoose, { Schema } from "mongoose";
+const agendaSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-    startTime:{
-        type:Date,
-        required:true,
+    description: {
+      type: String,
+      required: true,
     },
-    endTime:{
-        type:Date,
-        required:true,
+    startTime: {
+      type: Date,
+      required: true,
     },
-    eventId:{
-        type:Schema.Types.ObjectId,
-        ref:"Event",
-        required:true,
+    endTime: {
+      type: Date,
+      required: true,
     },
-    speakerId:{
-        type:Schema.Types.ObjectId,
-        ref:"Speaker",
-        required:true,
-    },
-    session:{
-        type:Schema.Types.ObjectId,
-        ref:"Session",
-        required:true,
-    },
+  },
+  { timestamps: true }
+);
+export const Agenda = mongoose.model("Agenda", agendaSchema);
 
-},{timestamps:true});
-export const Agenda = mongoose.model("Agenda",agendaSchema);
