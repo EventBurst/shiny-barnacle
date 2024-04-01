6db95f3ad6df15bb46e50bd4c1fb44691234c187
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-01 20:44:28 +0530

    refactored agenda model

--- models/agenda.model.js
+++ models/agenda.model.js
@@ -15,14 +15,17 @@ const agendaSchema = new Schema({
     eventId:{
         type:Schema.Types.ObjectId,
         ref:"Event",
+        required:true,
     },
     speakerId:{
         type:Schema.Types.ObjectId,
         ref:"Speaker",
+        required:true,
     },
     session:{
         type:Schema.Types.ObjectId,
         ref:"Session",
+        required:true,
     },
 
 },{timestamps:true});
