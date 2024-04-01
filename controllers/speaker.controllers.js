b55f61b3bbe22f52bca7e30c7dae4ac5aa15b015
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-01 22:11:37 +0530

    added route and controller for creating a speaker

--- controllers/speaker.controllers.js
+++ controllers/speaker.controllers.js
@@ -3,6 +3,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 import {ApiError} from "../utils/ApiError.js";
 import {ApiResponse} from "../utils/ApiResponse.js";
 import mongoose from "mongoose";
+
 //get all Speakers
 const getSpeakers = asyncHandler(async (req, res) => {
     const speakers = await Speaker.find();
@@ -10,4 +11,17 @@ const getSpeakers = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200, speakers, "Speakers retrieved successfully"));
   });
 
-  export {getSpeakers};
+// create a new Speaker
+const createSpeaker=asyncHandler(async(req,res)=>{
+  const {name,email,phoneNumber}=req.body;
+  if (
+    [name,email,phoneNumber].some((value) => value.trim() === "")
+  ) {
+    throw new ApiError(400, "All Fields are required");
+  }
+  const speaker=await Speaker.create({name,email,phoneNumber});
+  if(!speaker) throw new ApiError(400,"Speaker could not be created");
+  return res.status(201).json(new ApiResponse(201,speaker,"Speaker created successfully"));
+
+})
+  export {getSpeakers, createSpeaker};
--- routes/speaker.routes.js
+++ routes/speaker.routes.js
@@ -1,11 +1,14 @@
 import {Router} from 'express';
-import { getSpeakers } from '../controllers/speaker.controllers.js';
+import { createSpeaker, getSpeakers } from '../controllers/speaker.controllers.js';
 const router= Router();
 // Importing the controller
 
 // Defining the routes
+
 // Get all speakers
 router.get('/get-all', getSpeakers);
+// Create a new speaker
+router.post('/create', createSpeaker);
 
-
+// Exporting the router
 export default router;
