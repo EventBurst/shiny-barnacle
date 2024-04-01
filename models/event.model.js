89318d427d7b0612ca9204f4780f5ec64ebea03d
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-01 20:24:06 +0530

    Resolves #11

---
+++ models/event.model.js
@@ -0,0 +1,42 @@
+import mongoose, { Schema } from "mongoose";
+const eventSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    status: {
+      type: Enum,
+      required: true,
+      enum: ["draft", "published", "cancelled"],
+    },
+    sponsors: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Sponsor",
+      },
+    ],
+    sessions: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Session",
+      },
+    ],
+    organizer: {
+      type: Schema.Types.ObjectId,
+      ref: "Organizer",
+    },
+  },
+  { timestamps: true }
+);
+export const Event = mongoose.model("Event", eventSchema);
