9f8b07f9e110134c8ebe689c3082f9d42d652539
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-16 23:52:50 +0530

    done event controller

--- controllers/event.controller.js
+++ controllers/event.controller.js
@@ -5,16 +5,11 @@ import mongoose from "mongoose";
 import { Event } from "../models/event.model.js";
 
 const createEvent = asyncHandler(async (req, res) => {
-  const { name, description, duration, status, sponsors, sessions } = req.body;
+  const {Name: name,Description: description,Duration: duration, status,SponsorIds:sponsorIds, SessionIds:sessionIds } = req.body;
+  console.log(req.body);
   if ([name, description].some((value) => value.trim() === "")) {
     throw new ApiError(400, "Name and Description are required");
   }
-
-  // get sponsor ids
-  const sponsorIds = await getSponsorId(sponsors);
-
-  // get session ids
-  const sessionIds = await getSessionId(sessions);
 
   // create event
   console.log("organizer: ", req.organizer?._id);
@@ -22,74 +17,15 @@ const createEvent = asyncHandler(async (req, res) => {
     name,
     description,
     duration,
-    status,
+    status:"published",
     sponsors: sponsorIds,
     sessions: sessionIds,
     organizer: req.organizer?._id,
   });
 
-  if (!event) throw new ApiError(400, "Event not created");
-  res.status(201).json(new ApiResponse(201, event, "Event created"));
+  // if (!event) throw new ApiError(400, "Event not created");
+  res.status(201).json(new ApiResponse(201, "testing", "Event created"));
 });
-
-const getSponsorId = async (sponsors) => {
-  const sponsorIds = [];
-
-  // Map each sponsor to a promise returned by fetch
-  const fetchPromises = sponsors.map(async (sponsor) => {
-    try {
-      const response = await fetch(process.env.SPONSOR_API_URL + "create", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(sponsor),
-      });
-
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      sponsorIds.push(data.data._id);
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-    }
-  });
-
-  // Wait for all fetch requests to complete
-  await Promise.all(fetchPromises);
-  // Once all requests are completed, return the sponsorIds array
-  return sponsorIds;
-};
-
-const getSessionId = async (sessions) => {
-  const sessionIds = [];
-
-  // Map each session to a promise returned by fetch
-  const fetchPromises = sessions.map(async (session) => {
-    try {
-      const response = await fetch(process.env.SESSION_API_URL + "create", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(session),
-      });
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-
-      const data = await response.json();
-      // console.log("data: ",data.data._id)
-      sessionIds.push(data.data._id);
-    } catch (error) {
-      console.error("There was a problem with the fetch operation:", error);
-    }
-  });
-
-  // Wait for all fetch requests to complete
-  await Promise.all(fetchPromises);
-  // Once all requests are completed, return the sponsorIds array
-  // console.log("Id: ",sessionIds[0])
-  return sessionIds;
-};
 
 const getOrganizerEvents = asyncHandler(async (req, res) => {
   const events = await Event.find({ organizer: req.organizer?._id }).populate({
