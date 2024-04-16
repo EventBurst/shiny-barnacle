1d1852e6ee41fa0d0dc554b239e60c32e735561a
Developer-vansh <117535964+Developer-vansh@users.noreply.github.com>
2024-04-16 22:36:33 +0530

    refactored createSession controller

--- controllers/session.controller.js
+++ controllers/session.controller.js
@@ -26,62 +26,9 @@ const updateSession = asyncHandler(async (req, res) => {
 
 // create a new session
 const createSession = asyncHandler(async (req, res) => {
-  const { name, description, speaker, agenda } = req.body;
-
-  // if session exists
-  const sessionExists = await Session.findOne({ name });
-  if (sessionExists)
-    return res
-      .status(200)
-      .json(new ApiResponse(200, sessionExists, "Session already exists"));
-  // Stringify the speaker data before sending it
-  const speakerData = JSON.stringify(speaker);
-
-  let speakerId, agendaId;
-
-  // Make a POST request to create the speaker
-  await fetch(process.env.SPEAKER_API_URL + "create", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: speakerData, // Use the stringified speaker data
-  })
-    .then((response) => {
-      // Check if the response is successful (status 200)
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the response data according to your application logic
-      speakerId = data.data._id;
-    })
-    .catch((error) => {
-      // Handle any errors that occurred during the fetch
-      console.error("There was a problem with the fetch operation:", error);
-    });
-
-  // Make a POST request to create the agenda
-  await fetch(process.env.AGENDA_API_URL + "create", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(agenda), // Use the stringified agenda
-  })
-    .then((response) => {
-      // Check if the response is successful (status 200)
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      // Handle the response data according to your application logic
-      agendaId = data.data._id;
-    })
-    .catch((error) => {
-      console.error("There was a problem with the fetch operation:", error);
-    });
-
+  const {Name: name,Description: description,speakerId: speakerId, agendaId:agendaId } = req.body;
+  console.log(req.body);
+  
   // Create a new session
   const session = await Session.create({
     name,
@@ -92,7 +39,7 @@ const createSession = asyncHandler(async (req, res) => {
   if (!session) throw new ApiError(400, "Session was not created");
 
   // Send a success response to the client if every thing is ok
-  res.status(200).json(new ApiResponse(201, session, "success"));
+  res.status(200).json(new ApiResponse(201, "hii", "success"));
 });
 
 // Exporting the functions
